d55747cbe65e37a529f3a14751d8bf10c83f65b6
Armin Biere <biere@cs.uni-freiburg.de>
2022-04-14 14:35:11 +0200

    got basic stats working

--- gimbatul.c
+++ gimbatul.c
@@ -1,4 +1,4 @@
-// Copyright 2022 (c) Armin Biere, University of Freiburg, Germany
+// Copyright (c) 2022 Armin Biere University of Freiburg
 
 // *INDENT-OFF*
 
@@ -9,6 +9,7 @@ static const char * usage =
 "\n"
 "-h    print this command line option summary\n"
 "-n    do not print satisfying assignments\n"
+"-a    use ASCII proof format\n"
 "\n"
 "and '<dimacs>' is the path to the input DIMACS file ('<stdin>' if missing)\n"
 "and '<proof>' the path to the output 'DRAT' proof file.\n"
@@ -16,12 +17,82 @@ static const char * usage =
 
 // *INDENT-ON*
 
+/*------------------------------------------------------------------------*/
+
 #include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <inttypes.h>
+#include <pthread.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/*------------------------------------------------------------------------*/
+
+struct file
+{
+  const char * path;
+  FILE * file;
+  bool close;
+  size_t lines;
+};
+
+static struct file dimacs, proof;
+
+static double start;
+
+/*------------------------------------------------------------------------*/
+
+static double
+process_time (void)
+{
+  struct rusage u;
+  double res;
+  if (getrusage (RUSAGE_SELF, &u))
+    return 0;
+  res = u.ru_utime.tv_sec + 1e-6 * u.ru_utime.tv_usec;
+  res += u.ru_stime.tv_sec + 1e-6 * u.ru_stime.tv_usec;
+  return res;
+}
+
+static double
+wall_clock_time (void)
+{
+  struct timeval tv;
+  if (gettimeofday (&tv, 0))
+    return 0;
+  return 1e-6 * tv.tv_usec + tv.tv_sec;
+}
+
+static size_t
+maximum_resident_set_size (void)
+{
+  struct rusage u;
+  if (getrusage (RUSAGE_SELF, &u))
+    return 0;
+  return ((size_t) u.ru_maxrss) << 10;
+}
+
+static size_t
+current_resident_set_size (void)
+{
+  char path[48];
+  sprintf (path, "/proc/%d/statm", (int) getpid ());
+  FILE *file = fopen (path, "r");
+  if (!file)
+    return 0;
+  size_t dummy, rss;
+  int scanned = fscanf (file, "%zu %zu", &dummy, &rss);
+  fclose (file);
+  return scanned == 2 ? rss * sysconf (_SC_PAGESIZE) : 0;
+}
+
+/*------------------------------------------------------------------------*/
 
 static void die (const char*, ...)  __attribute__((format (printf, 1, 2)));
 
@@ -37,19 +108,55 @@ die (const char * fmt, ...)
   exit (1);
 }
 
-struct file
+static void fatal_error (const char*, ...)  __attribute__((format (printf, 1, 2)));
+
+static void
+fatal_error (const char * fmt, ...)
 {
-  const char * path;
-  FILE * file;
-  bool close;
-  long lines;
-};
+  fputs ("gimbatul: fatal error: ", stderr);
+  va_list ap;
+  va_start (ap, fmt);
+  vfprintf (stderr, fmt, ap);
+  va_end (ap);
+  fputc ('\n', stderr);
+  abort ();
+}
 
-static struct file dimacs, proof;
-static bool witness = true;
+static void parse_error (const char*, ...)  __attribute__((format (printf, 1, 2)));
 
-int
-main (int argc, char ** argv)
+static void
+parse_error (const char * fmt, ...)
+{
+  fprintf (stderr, "gimbatul: parse error: at line %zu in '%s': ",
+           dimacs.lines, dimacs.path);
+  va_list ap;
+  va_start (ap, fmt);
+  vfprintf (stderr, fmt, ap);
+  va_end (ap);
+  fputc ('\n', stderr);
+  exit (1);
+}
+
+static void message (const char*, ...)  __attribute__((format (printf, 1, 2)));
+
+static void
+message (const char * fmt, ...)
+{
+  fputs ("c ", stdout);
+  va_list ap;
+  va_start (ap, fmt);
+  vprintf (fmt, ap);
+  va_end (ap);
+  fputc ('\n', stdout);
+  fflush (stdout);
+}
+
+/*------------------------------------------------------------------------*/
+
+static bool witness = true, binary = true;
+
+static void
+parse_command_line_options (int argc, char ** argv)
 {
   for (int i = 1; i != argc; i++)
     {
@@ -61,6 +168,8 @@ main (int argc, char ** argv)
 	}
       else if (!strcmp (arg, "-n"))
 	witness = false;
+      else if (!strcmp (arg, "-a"))
+	binary = false;
       else if (arg[0] == '-' && arg[1])
 	die ("invalid option '%s' (try '-h')", arg);
       else if (proof.file)
@@ -86,6 +195,7 @@ main (int argc, char ** argv)
 	    {
 	      proof.path = "<stdout>";
 	      proof.file = stdout;
+	      binary = false;
 	    }
 	  else if (!(proof.file = fopen (arg, "w")))
 	    die ("can not open and write to '%s'", arg);
@@ -96,15 +206,61 @@ main (int argc, char ** argv)
 	    }
 	}
     }
+
   if (!dimacs.file)
     {
       dimacs.path = "<stdin>";
       dimacs.file = stdin;
     }
-  int res = 0;
+}
+
+static void
+print_banner (void)
+{
+  message ("Gimbatul SAT Solver");
+  message ("Copyright (c) 2022 Armin Biere University of Freiburg");
+}
+
+static void
+parse_dimacs_file ()
+{
+  assert (dimacs.file);
   if (dimacs.close)
     fclose (dimacs.file);
+}
+
+static void
+close_proof (void)
+{
+  if (!proof.file)
+    return;
   if (proof.close)
     fclose (proof.file);
+  message ("closing proof file '%s' after writing %zu lines",
+           proof.path, proof.lines);
+}
+
+static void
+print_statistics (void)
+{
+  double p = process_time ();
+  double w = wall_clock_time () - start;
+  double m = maximum_resident_set_size () / (double) (1<<20);
+  message ("%-30s%.2f sec", "process-time:", p);
+  message ("%-30s%.2f sec", "wall-clock-time:", w);
+  message ("%-30s%.2f MB", "maximum-resident-set-size:", m);
+}
+
+int
+main (int argc, char ** argv)
+{
+  start = wall_clock_time ();
+  parse_command_line_options (argc, argv);
+  print_banner ();
+  parse_dimacs_file ();
+  int res = 0;
+  close_proof ();
+  print_statistics ();
+  message ("exit %d", res);
   return res;
 }
