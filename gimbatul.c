8aac59cf2da68cf67198be826cfc4fe10f20c8d2
Armin Biere <biere@cs.uni-freiburg.de>
2022-04-23 22:25:19 +0200

    radix sort for reduce beta

--- gimbatul.c
+++ gimbatul.c
@@ -211,10 +211,12 @@ struct trail
 
 struct clause
 {
+#ifdef LOGGING
   size_t id:63;
+#endif
   bool redundant:1;
+  unsigned glue:31;
   unsigned shared;
-  unsigned glue;
   unsigned size;
   unsigned literals[];
 };
@@ -1301,6 +1303,8 @@ new_watch (struct solver *solver, struct clause *clause,
     watch->used = 1;
   else
     watch->used = 0;
+  if (glue > 255)
+    glue = 255;
   watch->glue = glue;
   watch->middle = 2;
   watch->sum = literals[0] ^ literals[1];
@@ -1326,7 +1330,9 @@ new_clause (struct solver *solver,
   assert (size <= solver->size);
   size_t bytes = size * sizeof (unsigned);
   struct clause *clause = allocate_block (sizeof *clause + bytes);
+#ifdef LOGGING
   clause->id = ++solver->statistics.added;
+#endif
   if (redundant)
     solver->statistics.redundant++;
   else
@@ -2036,6 +2042,8 @@ gather_reduce_candidates (struct solver *solver, struct watches *candidates)
 	   percent (SIZE (*candidates), solver->statistics.redundant));
 }
 
+#if 0
+
 static int
 cmp_reduce_candidates (const void *p, const void *q)
 {
@@ -2054,22 +2062,22 @@ cmp_reduce_candidates (const void *p, const void *q)
   if (h > 255)
     h = 255;
   if (g > h)
-    return -1;
-  if (g < h)
     return 1;
+  if (g < h)
+    return -1;
 #endif
   struct clause *c = u->clause;
   struct clause *d = v->clause;
 #if 0
   if (c->size > d->size)
-    return -1;
-  if (c->size < d->size)
     return 1;
+  if (c->size < d->size)
+    return -1;
 #endif
   if (c->id < d->id)
-    return -1;
-  if (c->id > d->id)
     return 1;
+  if (c->id > d->id)
+    return -1;
   assert (c == d);
   return 0;
 }
@@ -2083,15 +2091,60 @@ sort_reduce_candidates (struct watches *candidates)
     qsort (begin, size, sizeof *begin, cmp_reduce_candidates);
 }
 
+#else
+
+static void
+sort_reduce_candidates (struct watches * candidates)
+{
+  struct watch **begin = candidates->begin;
+  struct watch **end = candidates->begin;
+  size_t count[256];
+  for (struct watch ** p = begin; p != end; p++)
+    {
+      struct watch * watch = *p;
+      struct clause * clause = watch->clause;
+      unsigned glue = clause->glue;
+      if (glue > 255)
+	glue = 255;
+      count[glue]++;
+    }
+  size_t pos = 0;
+  for (unsigned i = 0; i != 256; i++)
+    {
+      size_t size = count[i];
+      count[i] = pos;
+      pos += size;
+    }
+  size_t size = end - begin;
+  size_t bytes = size * sizeof (struct watch*);
+  struct watch ** tmp = allocate_block (bytes);
+  for (struct watch ** p = begin; p != end; p++)
+    {
+      struct watch * watch = *p;
+      struct clause * clause = watch->clause;
+      unsigned glue = clause->glue;
+      if (glue > 255)
+	glue = 255;
+      tmp[count[glue]++] = watch;
+    }
+  memcpy (begin, tmp, bytes);
+  free (tmp);
+}
+
+#endif
+
 static void
 mark_reduce_candidates_as_garbage (struct solver *solver,
 				   struct watches *candidates)
 {
   size_t size = SIZE (*candidates);
   size_t target = REDUCE_FRACTION * size;
+  struct watch ** begin = candidates->begin;
+  struct watch ** p = candidates->end;
   size_t reduced = 0;
-  for (all_watches (watch, *candidates))
+  while (p != begin)
     {
+      struct watch * watch = *--p;
       LOGCLAUSE (watch->clause, "marking garbage");
       assert (!watch->garbage);
       watch->garbage = true;
