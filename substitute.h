cd880ac13166a9f78a28266e88d7b923328dc6e2
Armin Biere <biere@cs.uni-freiburg.de>
2022-05-14 08:17:28 +0200

    factored out substitute

---
+++ substitute.h
@@ -0,0 +1,10 @@
+#ifndef _substitute_h_INCLUDED
+#define _substitute_h_INCLUDED
+
+#include <stdbool.h>
+
+struct ruler;
+
+bool equivalent_literal_substitution (struct ruler *, unsigned round);
+
+#endif
