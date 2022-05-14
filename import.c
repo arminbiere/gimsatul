7074c86ce9e5d22a1ce75dd3f27b660423db4205
Armin Biere <biere@cs.uni-freiburg.de>
2022-05-14 11:24:09 +0200

    compiles with debugging again

---
+++ import.c
@@ -0,0 +1,400 @@
+#include "assign.h"
+#include "backtrack.h"
+#include "import.h"
+#include "random.h"
+#include "ruler.h"
+#include "trace.h"
+#include "utilities.h"
+
+static bool
+import_units (struct ring *ring)
+{
+  assert (ring->pool);
+  struct ruler *ruler = ring->ruler;
+#ifndef NFASTPATH
+  if (ring->units == ruler->units.end)
+    return false;
+#endif
+  struct variable *variables = ring->variables;
+  signed char *values = ring->values;
+  unsigned level = ring->level;
+  unsigned imported = 0;
+  if (pthread_mutex_lock (&ruler->locks.units))
+    fatal_error ("failed to acquire unit lock");
+  while (ring->units != ruler->units.end)
+    {
+      unsigned unit = *ring->units++;
+      LOG ("trying to import unit %s", LOGLIT (unit));
+      signed char value = values[unit];
+      if (level && value)
+	{
+	  unsigned idx = IDX (unit);
+	  if (variables[idx].level)
+	    value = 0;
+	}
+      if (value > 0)
+	continue;
+      very_verbose (ring, "importing unit %d", export_literal (unit));
+      ring->statistics.imported.units++;
+      ring->statistics.imported.clauses++;
+      imported++;
+      if (value < 0)
+	{
+	  set_inconsistent (ring, "imported falsified unit");
+	  trace_add_empty (&ring->buffer);
+	  imported = INVALID;
+	  break;
+	}
+      if (level)
+	{
+	  backtrack (ring, 0);
+	  level = 0;
+	}
+      assert (!ring->level);
+      assign_ring_unit (ring, unit);
+    }
+  if (pthread_mutex_unlock (&ruler->locks.units))
+    fatal_error ("failed to release unit lock");
+  return imported;
+}
+
+static void
+really_import_binary_clause (struct ring *ring, unsigned lit, unsigned other)
+{
+  (void) new_local_binary_clause (ring, true, lit, other);
+  trace_add_binary (&ring->buffer, lit, other);
+  ring->statistics.imported.binary++;
+  ring->statistics.imported.clauses++;
+}
+
+static void
+force_to_repropagate (struct ring *ring, unsigned lit)
+{
+  LOG ("forcing to repropagate %s", LOGLIT (lit));
+  assert (ring->values[lit] < 0);
+  unsigned idx = IDX (lit);
+  struct variable *v = ring->variables + idx;
+  if (ring->level > v->level)
+    backtrack (ring, v->level);
+  size_t pos = ring->trail.pos[idx];
+  assert (pos < SIZE (ring->trail));
+  LOG ("setting end of trail to %zu", pos);
+  unsigned *propagate = ring->trail.begin + pos;
+  assert (propagate < ring->trail.end);
+  assert (*propagate == NOT (lit));
+  ring->trail.propagate = propagate;
+}
+
+static bool
+subsumed_binary (struct ring *ring, unsigned lit, unsigned other)
+{
+  if (SIZE (REFERENCES (lit)) > SIZE (REFERENCES (other)))
+    SWAP (lit, other);
+  for (all_watches (watch, REFERENCES (lit)))
+    if (binary_pointer (watch) && other_pointer (watch) == other)
+      return true;
+  return false;
+}
+
+static bool
+import_binary (struct ring *ring, struct clause *clause)
+{
+  assert (binary_pointer (clause));
+  assert (redundant_pointer (clause));
+  signed char *values = ring->values;
+  unsigned lit = lit_pointer (clause);
+  signed char lit_value = values[lit];
+  unsigned lit_level = INVALID;
+  if (lit_value)
+    {
+      lit_level = VAR (lit)->level;
+      if (lit_value > 0 && !lit_level)
+	return false;
+    }
+  unsigned other = other_pointer (clause);
+  signed char other_value = values[other];
+  unsigned other_level = INVALID;
+  if (other_value)
+    {
+      other_level = VAR (other)->level;
+      if (other_value > 0 && !other_level)
+	return false;
+    }
+
+#define SUBSUME_BINARY(LIT, OTHER) \
+do { \
+  if (subsumed_binary (ring, LIT, OTHER)) \
+    { \
+      LOGBINARY (true, LIT, OTHER, "subsumed imported"); \
+      return false; \
+    } \
+} while (0);
+
+  if ((lit_value >= 0 && other_value >= 0) ||
+      (lit_value > 0 && other_value < 0 && lit_level <= other_level) ||
+      (other_value > 0 && lit_value < 0 && other_level <= lit_level))
+    {
+      SUBSUME_BINARY (lit, other);
+      LOGBINARY (true, lit, other, "importing (no propagation)");
+      really_import_binary_clause (ring, lit, other);
+      return false;
+    }
+
+  unsigned *pos = ring->trail.pos;
+  unsigned lit_pos = pos[IDX (lit)];
+  unsigned other_pos = pos[IDX (other)];
+
+  if (lit_value < 0 &&
+      (other_value >= 0 ||
+       lit_level < other_level ||
+       (lit_level == other_level && lit_pos < other_pos)))
+    {
+      SUBSUME_BINARY (lit, other);
+      LOGBINARY (true, lit, other,
+		 "importing (repropagate first literal %s)", LOGLIT (lit));
+      force_to_repropagate (ring, lit);
+      really_import_binary_clause (ring, lit, other);
+      return true;
+    }
+
+  assert (other_value < 0 &&
+	  (lit_value >= 0 ||
+	   other_level < lit_level ||
+	   (other_level == lit_level && other_pos < lit_pos)));
+
+  SUBSUME_BINARY (lit, other);
+  LOGBINARY (true, lit, other,
+	     "importing (repropagate second literal %s))", LOGLIT (other));
+  force_to_repropagate (ring, other);
+  really_import_binary_clause (ring, lit, other);
+  return true;
+}
+
+static bool
+subsumed_large_clause (struct ring *ring, struct clause *clause)
+{
+  signed char *values = ring->values;
+  struct variable *variables = ring->variables;
+  signed char *marks = ring->marks;
+  uint64_t min_watched = UINT64_MAX;
+  unsigned best = INVALID;
+  for (all_literals_in_clause (lit, clause))
+    {
+      signed char value = values[lit];
+      unsigned idx = IDX (lit);
+      struct variable *v = variables + idx;
+      if (value < 0 && !v->level)
+	continue;
+      assert (!value || v->level);
+      marks[lit] = 1;
+      size_t watched = SIZE (REFERENCES (lit));
+      if (watched >= min_watched)
+	continue;
+      min_watched = watched;
+      best = lit;
+    }
+  bool res = false;
+  if (best != INVALID)
+    {
+      struct references *watches = &REFERENCES (best);
+      for (all_watches (watch, *watches))
+	{
+	  if (binary_pointer (watch))
+	    continue;
+	  if (!watch->redundant)
+	    continue;
+	  res = true;
+	  struct clause *other_clause = watch->clause;
+	  for (all_literals_in_clause (other, other_clause))
+	    {
+	      if (other == best)
+		continue;
+	      signed char value = values[other];
+	      unsigned idx = IDX (other);
+	      struct variable *v = variables + idx;
+	      if (value < 0 && !v->level)
+		continue;
+	      signed char mark = marks[other];
+	      if (mark)
+		continue;
+	      res = false;
+	      break;
+	    }
+	  if (!res)
+	    continue;
+	  LOGCLAUSE (other_clause, "subsuming");
+	  break;
+	}
+    }
+  for (all_literals_in_clause (lit, clause))
+    marks[lit] = 0;
+  return res;
+}
+
+static void
+really_import_large_clause (struct ring *ring, struct clause *clause,
+			    unsigned first, unsigned second)
+{
+  watch_literals_in_large_clause (ring, clause, first, second);
+  unsigned glue = clause->glue;
+  assert (clause->redundant);
+  struct ring_statistics *statistics = &ring->statistics;
+  if (glue == 1)
+    statistics->imported.glue1++;
+  else if (glue <= TIER1_GLUE_LIMIT)
+    statistics->imported.tier1++;
+  else
+    {
+      assert (glue <= TIER2_GLUE_LIMIT);
+      statistics->imported.tier2++;
+    }
+  statistics->imported.clauses++;
+}
+
+static unsigned
+find_literal_to_watch (struct ring *ring, struct clause *clause,
+		       unsigned ignore, signed char *res_value_ptr,
+		       unsigned *res_level_ptr)
+{
+  signed char *values = ring->values;
+  unsigned res = INVALID, res_level = 0;
+  signed char res_value = 0;
+  for (all_literals_in_clause (lit, clause))
+    {
+      if (lit == ignore)
+	continue;
+      signed char lit_value = values[lit];
+      unsigned lit_level = VAR (lit)->level;
+      if (res != INVALID)
+	{
+	  if (lit_value < 0)
+	    {
+	      if (res_value >= 0)
+		continue;
+	      if (lit_level <= res_level)
+		continue;
+	    }
+	  else if (lit_value > 0)
+	    {
+	      if (res_value > 0 && lit_level >= res_level)
+		continue;
+	    }
+	  else
+	    {
+	      assert (!lit_value);
+	      if (res_value >= 0)
+		continue;
+	    }
+	}
+      res = lit;
+      res_level = lit_level;
+      res_value = lit_value;
+    }
+  *res_value_ptr = res_value;
+  *res_level_ptr = res_level;
+  return res;
+}
+
+static bool
+import_large_clause (struct ring *ring, struct clause *clause)
+{
+  signed char *values = ring->values;
+  for (all_literals_in_clause (lit, clause))
+    {
+      if (values[lit] <= 0)
+	continue;
+      if (VAR (lit)->level)
+	continue;
+      LOGCLAUSE (clause, "not importing %s satisfied", LOGLIT (lit));
+      dereference_clause (ring, clause);
+      return false;
+    }
+
+  unsigned lit_level = 0;
+  signed char lit_value = 0;
+  unsigned lit = find_literal_to_watch (ring, clause, INVALID,
+					&lit_value, &lit_level);
+  unsigned other_level = 0;
+  signed char other_value = 0;
+  unsigned other = find_literal_to_watch (ring, clause, lit,
+					  &other_value, &other_level);
+#define SUBSUME_LARGE_CLAUSE(CLAUSE) \
+do { \
+  if (subsumed_large_clause (ring, clause)) \
+    { \
+      dereference_clause (ring, clause); \
+      return false; \
+    } \
+} while (0)
+
+  if ((lit_value >= 0 && other_value >= 0) ||
+      (lit_value > 0 && other_value < 0 && lit_level <= other_level) ||
+      (other_value > 0 && lit_value < 0 && other_level <= lit_level))
+    {
+      SUBSUME_LARGE_CLAUSE (clause);
+      LOGCLAUSE (clause, "importing (no propagation)");
+      really_import_large_clause (ring, clause, lit, other);
+      return false;
+    }
+
+  unsigned lit_pos = ring->trail.pos[IDX (lit)];
+  unsigned other_pos = ring->trail.pos[IDX (other)];
+
+  if (lit_value < 0 &&
+      (other_value >= 0 ||
+       lit_level < other_level ||
+       (lit_level == other_level && lit_pos < other_pos)))
+    {
+      SUBSUME_LARGE_CLAUSE (clause);
+      LOGCLAUSE (clause, "importing (repropagate first watch %s)",
+		 LOGLIT (lit));
+      force_to_repropagate (ring, lit);
+      really_import_large_clause (ring, clause, lit, other);
+      return true;
+    }
+
+  assert (other_value < 0 &&
+	  (lit_value >= 0 ||
+	   other_level < lit_level ||
+	   (other_level == lit_level && other_pos < lit_pos)));
+
+  SUBSUME_LARGE_CLAUSE (clause);
+  LOGCLAUSE (clause, "importing (repropagate second watch %s)",
+	     LOGLIT (other));
+  force_to_repropagate (ring, other);
+  really_import_large_clause (ring, clause, lit, other);
+  return true;
+}
+
+bool
+import_shared (struct ring *ring)
+{
+  if (!ring->pool)
+    return false;
+  if (import_units (ring))
+    return true;
+  struct ruler *ruler = ring->ruler;
+  size_t rings = SIZE (ruler->rings);
+  assert (rings <= UINT_MAX);
+  assert (rings > 1);
+  unsigned id = random_modulo (&ring->random, rings - 1) + ring->id + 1;
+  if (id >= rings)
+    id -= rings;
+  assert (id < rings);
+  assert (id != ring->id);
+  struct ring *src = ruler->rings.begin[id];
+  assert (src->pool);
+  struct pool *pool = src->pool + ring->id;
+  struct clause *volatile *end = pool->share + SIZE_SHARED;
+  struct clause *clause = 0;
+  for (struct clause * volatile *p = pool->share; !clause && p != end; p++)
+#ifndef NFASTPATH
+    if (*p)
+#endif
+      clause = atomic_exchange (p, 0);
+  if (!clause)
+    return false;
+  if (binary_pointer (clause))
+    return import_binary (ring, clause);
+  return import_large_clause (ring, clause);
+}
