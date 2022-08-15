63eb9c3547a7a306e09bc6e663c013ea6e57f0f3
Armin Biere <biere@cs.uni-freiburg.de>
2022-08-15 07:24:44 +0200

    trying to make vivification more effective

--- vivify.c
+++ vivify.c
@@ -55,13 +55,15 @@ watched_vivification_candidate (struct watcher *watcher, unsigned tier)
     return false;
   if (tier == 1)
     {
+      if (!watcher->size)
+	return false;
       if (watcher->glue > TIER1_GLUE_LIMIT)
 	return false;
     }
   if (tier == 2)
     {
       if (watcher->glue <= TIER1_GLUE_LIMIT)
-	return false;
+	return !watcher->size;
       if (watcher->glue > TIER2_GLUE_LIMIT)
 	return false;
     }
