594347c8893ee6044b8a4509f532423264cf6b33
Armin Biere <biere@cs.uni-freiburg.de>
2023-05-03 11:14:41 +0200

    updated copyright

--- build.c
+++ build.c
@@ -8,7 +8,7 @@ void print_banner (void) {
   if (verbosity < 0)
     return;
   printf ("c GimSATul SAT Solver\n");
-  printf ("c Copyright (c) 2022 Armin Biere University of Freiburg\n");
+  printf ("c Copyright (c) 2022-2023 Armin Biere University of Freiburg\n");
   fputs ("c\n", stdout);
   printf ("c Version %s%s\n", VERSION, GITID ? " " GITID : "");
   printf ("c %s\n", COMPILER);
--- gimsatul.c
+++ gimsatul.c
@@ -1,5 +1,3 @@
-// Copyright (c) 2022 Armin Biere University of Freiburg
-
 #include "build.h"
 #include "catch.h"
 #include "clone.h"
