14f56b5a2e09e6b4053dc2753c8d3e4a2b6ad67b
Armin Biere <armin.biere@gmail.com>
2024-05-23 19:16:58 +0200

    updated sc2024 docker script

---
+++ aws/sc2024/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cat<<EOF>input.json
+{
+  "formula_file": "/rundir/$1",
+  "worker_node_ips": ["leader"],
+  "timeout_seconds": "1000",
+  "formula_language": "",
+  "solver_argument_list": [""]
+}
+EOF
+exec docker run -i --shm-size=32g --name leader --entrypoint bash --rm -v `pwd`:/rundir satcomp-gimsatul -c '/competition/solver; status=$?; cat /rundir/stdout.log; cat /rundir/stderr.log 1>&2; exit $status'
