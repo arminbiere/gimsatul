b0ed0f698ec9138a127972c8c9a40d14c00115bb
Armin Biere <armin.biere@gmail.com>
2025-06-09 15:35:28 +0200

    fixed watcher

--- vivify.c
+++ vivify.c
@@ -52,7 +52,7 @@ static inline bool watched_vivification_candidate (struct ring *ring,
     return false;
   if (!watcher->redundant)
     return false;
-  unsigned watcher_glue = watcher_glue;
+  unsigned watcher_glue = watcher->glue;
   unsigned tier1 = ring->tier1_glue_limit[0]; // NO TYPO!
   unsigned tier2 = ring->tier2_glue_limit[0]; // NO TYPO!
   if (tier == 1) {
